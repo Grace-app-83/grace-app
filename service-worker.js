e08b7f4984029b598c8de1e7b1d2fa4f06bfa7fb
Grace-app-83 <atulefidelia1963@gmail.com>
2026-03-13 02:25:37 +0000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'grace-v1';
+const urlsToCache = [
+  '/grace-app/app.html',
+  '/grace-app/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
+});
